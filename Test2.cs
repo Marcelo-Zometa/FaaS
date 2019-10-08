5a44560d5d365c383ef722787ab08157c733f47b
MarceloZometa2198 <43351819+MarceloZometa2198@users.noreply.github.com>
2019-10-08 14:36:47 -0600

    Add files via upload
    Function for adding Kaydon Stubbs to the database every 15 seconds

---
+++ Test2.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DBTest2Insert
+{
+    public static class Test2
+    {
+        [FunctionName("DatabaseCleanup")]
+        public static async Task Run([TimerTrigger("*/15 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "INSERT INTO dbo.Person " +
+                        "VALUES ('Kaydon', 'Stubbs')";
+
+
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var rows = await cmd.ExecuteNonQueryAsync();
+                    log.LogInformation($"{rows} rows were updated");
+                }
+            }
+        }
+    }
+}
