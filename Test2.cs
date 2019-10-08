eea1b7cc370000f6f2f03ccb11d9ad19ab45ab67
MarceloZometa2198 <43351819+MarceloZometa2198@users.noreply.github.com>
2019-10-08 17:19:28 -0600

    Add files via upload
    Marcelo didnt know what he was doing. He panicked and uploaded the whole project. That way everything is in Git, Now we clone

--- Test2.cs
+++ Test2.cs
@@ -9,26 +9,26 @@ namespace DBTest2Insert
 {
     public static class Test2
     {
-        [FunctionName("DatabaseCleanup")]
-        public static async Task Run([TimerTrigger("*/15 * * * * *")]TimerInfo myTimer, ILogger log)
-        {
-            // Get the connection string from app settings and use it to create a connection.
-            var str = Environment.GetEnvironmentVariable("sqldb_connection");
-            using (SqlConnection conn = new SqlConnection(str))
-            {
-                conn.Open();
-                var text = "INSERT INTO dbo.Person " +
-                        "VALUES ('Kaydon', 'Stubbs')";
+        //[FunctionName("DatabaseCleanup")]
+        //public static async Task Run([TimerTrigger("*/15 * * * * *")]TimerInfo myTimer, ILogger log)
+        //{
+        //    // Get the connection string from app settings and use it to create a connection.
+        //    var str = Environment.GetEnvironmentVariable("sqldb_connection");
+        //    using (SqlConnection conn = new SqlConnection(str))
+        //    {
+        //        conn.Open();
+        //        var text = "INSERT INTO dbo.Person " +
+        //                "VALUES ('Kaydon', 'Stubbs')";
 
 
 
-                using (SqlCommand cmd = new SqlCommand(text, conn))
-                {
-                    // Execute the command and log the # rows affected.
-                    var rows = await cmd.ExecuteNonQueryAsync();
-                    log.LogInformation($"{rows} rows were updated");
-                }
-            }
-        }
+        //        using (SqlCommand cmd = new SqlCommand(text, conn))
+        //        {
+        //            // Execute the command and log the # rows affected.
+        //            var rows = await cmd.ExecuteNonQueryAsync();
+        //            log.LogInformation($"{rows} rows were updated");
+        //        }
+        //    }
+        //}
     }
 }
